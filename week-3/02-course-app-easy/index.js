b9a67dc7ecd00eac2f514d9bfd4f4c6b68f8175c
Dux <kartikmadaan1907@gmail.com>
2023-08-11 14:15:58 +0530

    Completed backend without tokens

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const app = express();
 
 app.use(express.json());
-var number = 1;
 app.post("/admin/signup", function (req, res) {
   const input = req.body;
   fs.readFile("./solutions/admins.json", "utf-8", function (err, data) {
@@ -26,7 +25,7 @@ app.post("/admin/signup", function (req, res) {
             else res.status(200).send("Account Created Successfully!");
           }
         );
-      } else res.status(403).send("User already exists!");
+      } else res.status(403).send("Admin already exists!");
     }
   });
 });
@@ -63,7 +62,7 @@ app.post("/admin/courses", function (req, res) {
           else {
             const courses = JSON.parse(data);
             if (!body.title || !body.description || !body.price)
-              res.send('Either of the title,description or price is missing!')
+              res.send("Either of the title,description or price is missing!");
             else {
               body.id = courses.length + 1;
               courses.push(body);
@@ -85,67 +84,187 @@ app.post("/admin/courses", function (req, res) {
   });
 });
 
-app.put('/admin/courses/:courseId', function (req, res) {
-  var { username, password } = req.headers
-  var body = req.body
-  fs.readFile('./solutions/admins.json', 'utf-8', function (err, data) {
-    if (err) throw data
+app.put("/admin/courses/:courseId", function (req, res) {
+  var { username, password } = req.headers;
+  var body = req.body;
+  fs.readFile("./solutions/admins.json", "utf-8", function (err, data) {
+    if (err) throw data;
     else {
-      var admins = JSON.parse(data)
-      var final = admins.findIndex(index => {
-        return index.username == username && index.password == password
-      })
-      if (final == -1)
-        res.status(203).send('Wrong credentials!')
+      var admins = JSON.parse(data);
+      var final = admins.findIndex((index) => {
+        return index.username == username && index.password == password;
+      });
+      if (final == -1) res.status(203).send("Wrong credentials!");
       else {
-        fs.readFile('./solutions/courses.json', 'utf-8', function (err, data) {
-          if (err) throw err
+        fs.readFile("./solutions/courses.json", "utf-8", function (err, data) {
+          if (err) throw err;
           else {
-            var courses = JSON.parse(data)
-            body.id = courses.length + 1
-            var id = req.params.courseId
-            console.log(id)
-            var courseIndex = courses.findIndex(index => index.id == id)
+            var courses = JSON.parse(data);
+            body.id = courses.length + 1;
+            var id = req.params.courseId;
+            // console.log(id);
+            var courseIndex = courses.findIndex((index) => index.id == id);
             if (courseIndex != -1) {
-              courses = courses.map(index => index.id == id ? { ...index, ...body } : index)
-              console.log(courses + 'hi')
-              fs.writeFile('./solutions/courses.json', JSON.stringify(courses), 'utf-8', function (err) {
-                if (err) throw err
-                else
-                  res.send('Updated successfully!')
-              })
-            }
-            else
-              res.status(404).send('Course with mentioned id not found!')
+              courses = courses.map((index) =>
+                index.id == id ? { ...index, ...body } : index
+              );
+              console.log(courses + "hi");
+              fs.writeFile(
+                "./solutions/courses.json",
+                JSON.stringify(courses),
+                "utf-8",
+                function (err) {
+                  if (err) throw err;
+                  else res.send("Updated successfully!");
+                }
+              );
+            } else res.status(404).send("Course with mentioned id not found!");
           }
-        })
+        });
       }
     }
-  })
-})
+  });
+});
 
-app.get('/admin/courses', function (req, res) {
-  const { username, password } = req.headers
-  fs.readFile('./solutions/admins.json', 'utf-8', function (err, data) {
-    if (err) throw err
+app.get("/admin/courses", function (req, res) {
+  const { username, password } = req.headers;
+  fs.readFile("./solutions/admins.json", "utf-8", function (err, data) {
+    if (err) throw err;
     else {
-      var admins = JSON.parse(data)
-    var final = admins.findIndex(index => index.username == username && index.password == password)
-    if (final != -1) {
-      fs.readFile('./solutions/courses.json', 'utf-8', function (err, data) {
-        if (err) throw err
-        else {
-          var courses = JSON.parse(data)
-          res.send(courses)
-        }
-      })
+      var admins = JSON.parse(data);
+      var final = admins.findIndex(
+        (index) => index.username == username && index.password == password
+      );
+      if (final != -1) {
+        fs.readFile("./solutions/courses.json", "utf-8", function (err, data) {
+          if (err) throw err;
+          else {
+            var courses = JSON.parse(data);
+            res.send(courses);
+          }
+        });
+      } else res.status(203).send("Wrong credentials!");
     }
-    else
-      res.status(203).send('Wrong credentials!')
+  });
+});
+app.post("/users/signup", function (req, res) {
+  const input = req.body;
+  fs.readFile("./solutions/users.json", "utf-8", function (err, data) {
+    if (err) throw err;
+    else {
+      const users = JSON.parse(data);
+      const final = users.findIndex((index) => {
+        return (
+          index.username == input.username && index.password == input.password
+        );
+      });
+      if (final == -1) {
+        users.push(input);
+        fs.writeFile(
+          "./solutions/users.json",
+          JSON.stringify(users),
+          "utf-8",
+          function (err) {
+            if (err) throw err;
+            else res.status(200).send("Account Created Successfully!");
+          }
+        );
+      } else res.status(403).send("User already exists!");
     }
-  })
-  
-})
+  });
+});
+app.post("/users/login", function (req, res) {
+  const input = req.headers;
+  fs.readFile("./solutions/users.json", "utf-8", function (err, data) {
+    if (err) throw err;
+    else {
+      const users = JSON.parse(data);
+      const final = users.findIndex((index) => {
+        return (
+          index.username == input.username && index.password == input.password
+        );
+      });
+      if (final != -1) res.send("Login successful!");
+      else res.status(404).send("Wrong username or password");
+    }
+  });
+});
+
+app.get("/users/courses", function (req, res) {
+  const { username, password } = req.headers;
+  fs.readFile("./solutions/users.json", "utf-8", function (err, data) {
+    if (err) throw err;
+    else {
+      var users = JSON.parse(data);
+      var final = users.findIndex(
+        (index) => index.username == username && index.password == password
+      );
+      if (final != -1) {
+        fs.readFile("./solutions/courses.json", "utf-8", function (err, data) {
+          if (err) throw err;
+          else {
+            var courses = JSON.parse(data);
+            courses = courses.filter((course) => course.published);
+            res.send(courses);
+          }
+        });
+      } else res.status(203).send("Wrong credentials!");
+    }
+  });
+});
+
+app.post("/users/courses/:courseId", function (req, res) {
+  var { username, password } = req.headers;
+  var id = req.params.courseId;
+  fs.readFile("./solutions/users.json", "utf-8", function (err, data) {
+    if (err) throw err;
+    else {
+      var users = JSON.parse(data);
+      var final = users.findIndex(
+        (index) => index.username == username && index.password == password
+      );
+      if (final == -1) res.send("Wrong credentials!");
+      else {
+        fs.readFile("./solutions/courses.json", "utf-8", function (err, data) {
+          if (err) throw data;
+          else {
+            const courses = JSON.parse(data);
+            var course = courses.filter((course) => course.id == id);
+            if (course) {
+              users[final].purchasedCourses.push(course);
+              fs.writeFile(
+                "./solutions/users.json",
+                JSON.stringify(users),
+                "utf-8",
+                function (err) {
+                  if (err) throw err;
+                  else res.send("Purchased Successful!");
+                }
+              );
+            } else res.send("Invalid Id!");
+          }
+        });
+      }
+    }
+  });
+});
+
+app.get("/users/purchasedCourses", function (req, res) {
+  var { username, password } = req.headers;
+  fs.readFile("./solutions/users.json", "utf-8", function (err, data) {
+    if (err) throw err;
+    else {
+      var users = JSON.parse(data);
+      var final = users.findIndex(
+        (index) => index.username == username && index.password == password
+      );
+      if (final == -1) res.send("Wrong credentials!");
+      else {
+        res.send(users[final].purchasedCourses);
+      }
+    }
+  });
+});
 app.all("*", (req, res) => {
   res.status(404).send("Page Not found!");
 });
