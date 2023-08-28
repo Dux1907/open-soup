0f94e188eb03d9ca443f75b0399ae69c4ffdc916
Dux <kartikmadaan1907@gmail.com>
2023-08-28 15:33:48 +0530

    Version 2

---
+++ updating/version2.js
@@ -0,0 +1,52 @@
+function createElement(todo) {
+    let updated = 0,added = 0,removed = 0
+    var parent = document.getElementById('main')
+    currentChildren = Array.from(parent.children)
+
+    todo.forEach((item) => {
+        var existingChild = currentChildren.find((child) => {
+            return child.dataset.id = item.id
+        })
+        if (existingChild) {
+            updated++
+            currentChildren = currentChildren.filter((child) => child.dataset.id != item.id)
+            existingChild.children[0].innerHTML = item.title + ' '
+            existingChild.children[1].innerHTML = item.description + ' '
+        }
+        else {
+            added++;
+            const child = document.createElement('div')
+            const child1 = document.createElement('span')
+          const child2 = document.createElement('span')
+          const child3 = document.createElement('span')
+             child1.innerHTML = item.title + ' '
+            child2.innerHTML = item.description + ' '
+            child3.innerHTML = item.id
+            child.appendChild(child1)
+            child.appendChild(child2)
+            child.appendChild(child3)
+             parent.appendChild(child)
+        }
+    })
+    currentChildren.forEach((child) => {
+        removed++;
+        parent.removeChild(child)
+    })
+
+    console.log('Updated: ' + updated)
+    console.log('Added: ' + added)
+    console.log('Removed: ' + removed)
+}
+
+setInterval(() => {
+    let todo = []
+    let k = Math.floor(Math.random() * 100)
+    for (let i = 0; i < k; i++){
+        todo.push({
+            title: 'Gym',
+            description: 'Go to gym at 5',
+            id: i + 1
+        })
+    }
+    createElement(todo)
+},4000)
