411539434da09de40de42640b8f658c0be34a633
Dux <kartikmadaan1907@gmail.com>
2023-09-29 03:31:04 +0530

    Route for Addcourses

--- Week-4-Assignments/02-medium-course-selling-app-admin-dashboard/src/App.jsx
+++ Week-4-Assignments/02-medium-course-selling-app-admin-dashboard/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Signup from "./assets/components/Signup";
 import Login from "./assets/components/Login";
+import AddCourses from "./assets/components/addCourses";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path = '/courses' element = {<AddCourses />}></Route>
       </Routes>
     </>
   );
