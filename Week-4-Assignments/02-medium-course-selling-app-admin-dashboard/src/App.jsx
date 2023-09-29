f333c96c274f9968aa2c940990684722d5d5cbe1
Dux <kartikmadaan1907@gmail.com>
2023-09-30 02:13:58 +0530

    Added Routes

--- Week-4-Assignments/02-medium-course-selling-app-admin-dashboard/src/App.jsx
+++ Week-4-Assignments/02-medium-course-selling-app-admin-dashboard/src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./assets/components/Signup";
 import Login from "./assets/components/Login";
 import AddCourses from "./assets/components/addCourses";
+import Courses from './assets/components/Courses'
+import UpdateCourse from "./assets/components/UpdateCourse";
 
 const App = () => {
   return (
     <>
-      <Routes>
+          <Routes>
         <Route path="/signup" element={<Signup />}></Route>
               <Route path="/login" element={<Login />}></Route>
-              <Route path = '/courses' element = {<AddCourses />}></Route>
+              <Route path = 'courses' element = {<Courses/>}></Route>
+              <Route path='/addcourses' element={<AddCourses />}></Route>
+              <Route path = '/updatecourse/:courseId' element = {<UpdateCourse />}></Route>
       </Routes>
     </>
   );
