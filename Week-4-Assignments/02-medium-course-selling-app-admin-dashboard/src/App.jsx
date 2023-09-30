25c44f3406d2746266cef66c1f54791d6e4af92e
Dux <kartikmadaan1907@gmail.com>
2023-10-01 00:57:15 +0530

    Added routing

--- Week-4-Assignments/02-medium-course-selling-app-admin-dashboard/src/App.jsx
+++ Week-4-Assignments/02-medium-course-selling-app-admin-dashboard/src/App.jsx
@@ -1,19 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import Signup from "./assets/components/Signup";
-import Login from "./assets/components/Login";
-import AddCourses from "./assets/components/addCourses";
-import Courses from './assets/components/Courses'
-import UpdateCourse from "./assets/components/UpdateCourse";
+import SignupAdmin from "./assets/components/Admin/Signup";
+import LoginAdmin from "./assets/components/Admin/Login";
+import AddCourses from "./assets/components/Admin/AddCourses";
+import CoursesAdmin from "./assets/components/Admin/Courses";
+import UpdateCourse from "./assets/components/Admin/UpdateCourse";
+import SignupUser from "./assets/components/Users/Signup";
+import LoginUser from "./assets/components/Users/Login";
+import CoursesUser from './assets/components/Users/Courses'
+import PurchasedCourses from "./assets/components/Users/PurchasedCourses";
 
 const App = () => {
   return (
     <>
-          <Routes>
-        <Route path="/signup" element={<Signup />}></Route>
-              <Route path="/login" element={<Login />}></Route>
-              <Route path = 'courses' element = {<Courses/>}></Route>
-              <Route path='/addcourses' element={<AddCourses />}></Route>
-              <Route path = '/updatecourse/:courseId' element = {<UpdateCourse />}></Route>
+      <Routes>
+        <Route path="/admin/signup" element={<SignupAdmin />}></Route>
+        <Route path="/admin/login" element={<LoginAdmin />}></Route>
+        <Route path="/admin/courses" element={<CoursesAdmin />}></Route>
+        <Route path="/admin/addcourses" element={<AddCourses />}></Route>
+        <Route
+          path="/admin/updatecourse/:courseId"
+          element={<UpdateCourse />}
+        ></Route>
+        <Route path="/user/signup" element={<SignupUser />}></Route>
+
+        <Route path="/user/login" element={<LoginUser />}></Route>
+        <Route path='/user/courses' element={<CoursesUser />}></Route>
+        <Route path = '/user/purchasedcourses' element = {<PurchasedCourses/>}></Route>
+        <Route path="*" element={<SignupUser />}></Route>
       </Routes>
     </>
   );
